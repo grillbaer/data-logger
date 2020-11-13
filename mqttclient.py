12e7652c93a557600bde757300c72fe2ed8f914c
Holger Fleischmann <holgflei@gmail.com>
2020-11-13 19:25:17 +0100

    publish mqtt only every 10 seconds

--- mqttclient.py
+++ mqttclient.py
@@ -9,11 +9,11 @@ __license__ = 'Apache License 2.0'
 import logging
 import json
 from datetime import datetime
-from functools import partial
 
 import paho.mqtt.client as mqtt
 
-from signalsources import SignalSource, SignalValue
+from signalsources import SignalSource
+from utils import RepeatTimer
 
 logger = logging.getLogger().getChild(__name__) 
 
@@ -22,6 +22,8 @@ class MqttClient:
     """
     Sends all signal changes to a MQTT broker.
     """
+
+    DELTA_SECONDS_DEFAULT = 10  # seconds
 
     def __init__(self):
         self.broker_host = 'localhost'
@@ -34,6 +36,9 @@ class MqttClient:
         self.client.on_disconnect = self._on_disconnect
         self.client.on_message = self._on_message
         self.__started = False
+        self.__timer = None
+        self.delta_seconds = MqttClient.DELTA_SECONDS_DEFAULT
+        self.sources = []
     
     def use_signals_config(self, signal_sources_config):
         self.broker_host = signal_sources_config['mqtt_broker_host']
@@ -42,8 +47,7 @@ class MqttClient:
         self.broker_base_topic = signal_sources_config['mqtt_broker_base_topic']
         for group in signal_sources_config['groups']:
             for source in group['sources']:
-                topic = self.broker_base_topic + '/' + source.identifier
-                source.add_callback(partial(self._publish_signal_value, source))
+                self.sources.append(source)
     
     def start(self):
         if not self.__started:
@@ -55,26 +59,34 @@ class MqttClient:
                     self.client.username_pw_set(self.broker_user, self.broker_password)
                 self.client.connect_async(self.broker_host)
                 self.client.loop_start()
+                self.__timer = RepeatTimer(self.delta_seconds, self.publish)
+                self.__timer.start()
                 self.__started = True
-        
+
     def stop(self):
         if self.__started:
             logger.info("Stopping MQTT client for broker " + self.broker_host)
             self.__started = False
+            self.__timer.cancel()
+            self.__timer = None
             self.client.disconnect()
             self.client.loop_stop(True)
     
-    def _publish_signal_value(self, source, signal_value):
-        if self.__started:
-            topic = self.broker_base_topic + '/' + source.identifier
-            json_value = json.dumps({
-                'value':     signal_value.value,
-                'status':    signal_value.status,
-                'formatted': '---' if signal_value.status != SignalSource.STATUS_OK else source.format(signal_value.value),
-                'timestamp': datetime.fromtimestamp(signal_value.timestamp).isoformat(),
-                'unit':      source.unit
-            })
-            self.client.publish(topic, json_value, 0, True)
+    def publish(self):
+        for source in self.sources:
+            signal_value = source.last_value
+            if signal_value is not None:
+                topic = self.broker_base_topic + '/' + source.identifier
+                json_value = json.dumps({
+                    'value':     signal_value.value,
+                    'status':    signal_value.status,
+                    'formatted':
+                        '---' if signal_value.status != SignalSource.STATUS_OK
+                        else source.format(signal_value.value),
+                    'timestamp': datetime.fromtimestamp(signal_value.timestamp).isoformat(),
+                    'unit':      source.unit
+                })
+                self.client.publish(topic, json_value, 0, True)
         
     def _on_connect(self, client, userdata, flags, rc):
         if rc == 0:
